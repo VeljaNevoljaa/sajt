c631f2fa0b54e9411132ff08a6666c6b255511fd
VeljaNevoljaa <veljkobelivuk1@gmail.com>
2025-06-17 22:11:21 +0200

    Add files via upload

---
+++ appdata (primenjene)/functions primenjene.js
@@ -0,0 +1,12 @@
+﻿function fun(x) {
+
+    switch (x) {
+        case 1: window.open("../saznajvise/klikometar.html", "", "top=500,left=1000,width=700 height=500"); break;
+        case 2: window.open("../saznajvise/lavirint.html", "", "top=500,left=1000,width=700 height=500"); break;
+        case 3: window.open("../saznajvise/flappy bird.html", "", "top=500,left=1000,width=700 height=500"); break;
+        case 4: window.open("../saznajvise/lovac.html", "", "top=500,left=1000,width=700 height=500"); break;
+        case 5: window.open("../saznajvise/pucacina.html", "", "top=500,left=1000,width=700 height=500"); break;
+        case 6: window.open("../saznajvise/brickbreaker.html", "", "top=500,left=1000,width=700 height=500"); break;
+        case 7: window.open("../saznajvise/agario.html", "", "top=500,left=1000,width=700 height=500"); break;
+    }
+}
---
+++ appdata/functions.js
@@ -0,0 +1,6 @@
+﻿const menuToggle = document.getElementById('menuToggle');
+const navLinks = document.getElementById('navLinks');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
